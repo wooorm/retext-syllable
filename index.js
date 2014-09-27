a46c295f3427b64cefb584b4521ace82da80fd5e
wooorm <tituswormer@gmail.com>
2014-09-27 14:58:30 +0200

    Refactor API

--- index.js
+++ index.js
@@ -1,40 +1,86 @@
 'use strict';
 
-exports = module.exports = function () {};
+/**
+ * Dependencies.
+ */
 
-var syllable = require('syllable');
+var getSyllableCount;
 
-function onchange(node, sillableDifference) {
-    if (!node || !sillableDifference) {
+getSyllableCount = require('syllable');
+
+/**
+ * Define `syllable`.
+ */
+
+function syllable() {}
+
+/**
+ * Change handler.
+ *
+ * @param {Node} node
+ * @param {number} difference
+ */
+
+function onchange(node, difference) {
+    if (!node || !difference) {
         return;
     }
 
-    node.data.syllableCount =
-        (node.data.syllableCount || 0) + sillableDifference;
+    node.data.syllableCount = (node.data.syllableCount || 0) + difference;
 
-    onchange(node.parent, sillableDifference);
+    onchange(node.parent, difference);
 }
+
+/**
+ * Handler for node insertions.
+ *
+ * @this {Child}
+ */
 
 function oninsert() {
     onchange(this.parent, this.data.syllableCount || 0);
 }
 
+/**
+ * Handler for node deletions.
+ *
+ * @this {Child}
+ */
+
 function onremove(previousParent) {
     onchange(previousParent, -(this.data.syllableCount || 0));
 }
 
+/**
+ * Handler for node value changes.
+ *
+ * @this {WordNode}
+ */
+
 function onchangetextinside() {
-    var self = this,
-        currentSyllableCount = self.data.syllableCount || 0,
-        sillableCount = syllable(self.toString());
+    var self,
+        currentCount,
+        syllableCount;
 
-    self.data.syllableCount = sillableCount;
+    self = this;
+    currentCount = self.data.syllableCount || 0;
+    syllableCount = getSyllableCount(self.toString());
 
-    onchange(self.parent, -currentSyllableCount + sillableCount);
+    self.data.syllableCount = syllableCount;
+
+    onchange(self.parent, -currentCount + syllableCount);
 }
 
+/**
+ * Define `attach`.
+ *
+ * @param {Retext} retext;
+ */
+
 function attach(retext) {
-    var TextOM = retext.TextOM;
+    var TextOM;
+
+    TextOM = retext.TextOM;
 
     TextOM.WordNode.on('changetextinside', onchangetextinside);
     TextOM.WordNode.on('removeinside', onchangetextinside);
@@ -43,4 +89,14 @@ function attach(retext) {
     TextOM.Child.on('remove', onremove);
 }
 
-exports.attach = attach;
+/**
+ * Expose `attach`.
+ */
+
+syllable.attach = attach;
+
+/**
+ * Expose `syllable`.
+ */
+
+module.exports = syllable;
