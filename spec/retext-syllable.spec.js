59fe989f611077e5231d67753e707fe1c75d780b
wooorm <tituswormer@gmail.com>
2014-09-27 14:50:56 +0200

    Refactor spec

--- spec/retext-syllable.spec.js
+++ spec/retext-syllable.spec.js
@@ -1,16 +1,14 @@
 'use strict';
 
+/**
+ * Dependencies.
+ */
+
 var syllable,
     content,
     visit,
     Retext,
-    assert,
-    tree,
-    counts,
-    parentCount,
-    otherWords,
-    otherCounts,
-    otherParentCount;
+    assert;
 
 syllable = require('..');
 content = require('retext-content');
@@ -18,33 +16,68 @@ Retext = require('retext');
 visit = require('retext-visit');
 assert = require('assert');
 
-tree = new Retext()
+/**
+ * Retext.
+ */
+
+var retext;
+
+retext = new Retext()
     .use(visit)
     .use(content)
-    .use(syllable)
-    .parse('A simple, english, sentence');
+    .use(syllable);
 
+/**
+ * Fixtures.
+ */
+
+var sentence,
+    counts,
+    parentCount,
+    otherWords,
+    otherCounts,
+    otherParentCount;
+
+sentence = 'A simple, english, sentence';
 counts = [1, 2, 2, 2];
 otherWords = ['A', 'yellow', 'fresh', 'banana'];
 otherCounts = [1, 2, 1, 3];
 parentCount = 7;
 otherParentCount = 7;
 
+/**
+ * Tests.
+ */
+
 describe('syllable()', function () {
-    it('should be of type `function`', function () {
+    var tree;
+
+    before(function (done) {
+        retext.parse(sentence, function (err, node) {
+            tree = node;
+
+            done(err);
+        });
+    });
+
+    it('should be a `function`', function () {
         assert(typeof syllable === 'function');
     });
 
-    it('should export an attach method of type `function`', function () {
+    it('should export an `attach` method', function () {
         assert(typeof syllable.attach === 'function');
     });
 
     it('should count each `WordNode`', function () {
-        var iterator = -1;
+        var index;
 
-        tree.visitType(tree.WORD_NODE, function (wordNode) {
-            assert('syllableCount' in wordNode.data);
-            assert(wordNode.data.syllableCount === counts[++iterator]);
+        index = -1;
+
+        tree.visitType(tree.WORD_NODE, function (node) {
+            index++;
+
+            assert('syllableCount' in node.data);
+            assert(node.data.syllableCount === counts[index]);
         });
     });
 
@@ -54,12 +87,13 @@ describe('syllable()', function () {
         assert(tree.head.head.data.syllableCount === parentCount);
     });
 
-    it('should set each `syllableCount` to `0` when a WordNode (no ' +
-        'longer?) has a value', function () {
-            tree.visitType(tree.WORD_NODE, function (wordNode) {
-                wordNode.removeContent();
+    it('should set each `syllableCount` to `0` when a `WordNode` has no ' +
+        'value',
+        function () {
+            tree.visitType(tree.WORD_NODE, function (node) {
+                node.removeContent();
 
-                assert(wordNode.data.syllableCount === 0);
+                assert(node.data.syllableCount === 0);
             });
         }
     );
@@ -70,17 +104,19 @@ describe('syllable()', function () {
         assert(tree.head.head.data.syllableCount === 0);
     });
 
-    it('should automatically re-count `WordNode`s when their values change',
-        function () {
-            var iterator = -1;
+    it('should re-count `WordNode`s when their values change', function () {
+        var index;
 
-            tree.visitType(tree.WORD_NODE, function (wordNode) {
-                wordNode.replaceContent(otherWords[++iterator]);
+        index = -1;
 
-                assert(wordNode.data.syllableCount === otherCounts[iterator]);
-            });
-        }
-    );
+        tree.visitType(tree.WORD_NODE, function (node) {
+            index++;
+
+            node.replaceContent(otherWords[index]);
+
+            assert(node.data.syllableCount === otherCounts[index]);
+        });
+    });
 
     it('should re-count `syllableCount` in parents', function () {
         assert(tree.data.syllableCount === otherParentCount);
